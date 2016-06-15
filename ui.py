7bc9864d9c3f4562d0d613f2106612f510ba88f2
tsonnen <tsonnen67@gmail.com>
2016-06-14 19:17:48 -0700

    Update the display of the information

--- ui.py
+++ ui.py
@@ -1,59 +1,59 @@
-from Tkinter import *
+import Tkinter as tk
 import json
 import charGen
 
-class App:
+class App(tk.Frame):
 
-    def __init__(self, master):
-        self.master = master
-        self.system  = StringVar(master)
-        self.setting = StringVar(master)
-        self.classChoice = StringVar(master)
-        self.race = StringVar(master)
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+
+        self.system  = tk.StringVar(self)
+        self.setting = tk.StringVar(self)
+        self.classChoice = tk.StringVar(self)
+        self.race = tk.StringVar(self)
         self.system.set("2d6")
         self.setting.set("Fantasy")
         self.classChoice.set("Scout")
 
-        frame = Frame(master)
-        frame.grid()
+        self.grid()
 
-        Label(master, text = "Stats").grid(row = 0, column = 0, sticky=W)
+        tk.Label(self, text = "Stats").grid(row = 0, column = 0, sticky=tk.W)
 
-        self.var1 = IntVar(master)
-        Checkbutton(master, text="Strength", variable = self.var1).grid(row = 1, column = 0, sticky=W)
+        self.var1 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Strength", variable = self.var1).grid(row = 1, column = 0, sticky=tk.W)
 
-        self.var2 = IntVar(master)
-        Checkbutton(master, text="Dexterity", variable = self.var2).grid(row = 2, column = 0, sticky=W)
+        self.var2 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Dexterity", variable = self.var2).grid(row = 2, column = 0, sticky=tk.W)
 
-        self.var3 = IntVar(master)
-        Checkbutton(master, text="Intelligence", variable = self.var3).grid(row = 3, column = 0, sticky=W)
+        self.var3 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Intelligence", variable = self.var3).grid(row = 3, column = 0, sticky=tk.W)
 
-        self.var4 = IntVar(master)
-        Checkbutton(master, text="Constitution", variable = self.var4).grid(row = 4, column = 0, sticky=W)
+        self.var4 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Constitution", variable = self.var4).grid(row = 4, column = 0, sticky=tk.W)
 
-        self.var5 = IntVar(master)
-        Checkbutton(master, text="Wisdom", variable = self.var5).grid(row = 5, column = 0, sticky=W)
+        self.var5 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Wisdom", variable = self.var5).grid(row = 5, column = 0, sticky=tk.W)
 
-        self.var6 = IntVar(master)
-        Checkbutton(master, text="Charisma", variable = self.var6).grid(row = 6, column = 0, sticky=W)
+        self.var6 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Charisma", variable = self.var6).grid(row = 6, column = 0, sticky=tk.W)
 
-        self.var7 = IntVar(master)
-        Checkbutton(master, text="Psionics", variable = self.var7).grid(row = 7, column = 0, sticky=W)
+        self.var7 = tk.IntVar(self)
+        tk.Checkbutton(self, text="Psionics", variable = self.var7).grid(row = 7, column = 0, sticky=tk.W)
 
-        Label(master, text  = "System").grid(row = 0, column = 2, sticky=W)
-        OptionMenu(master, self.system, "2d6", "4d6", "Dice").grid(row = 1, column = 2, sticky=W)
+        tk.Label(self, text  = "System").grid(row = 0, column = 2, sticky=tk.W)
+        tk.OptionMenu(self, self.system, "2d6", "4d6", "Dice").grid(row = 1, column = 2, sticky=tk.W)
 
-        Label(master, text = "Setting").grid(row = 2, column = 2, sticky=W)
-        OptionMenu(master, self.setting, "Fantasy", "Sci-Fi", "IRL", 
-                   command = lambda x: self.updateMenus()).grid(row = 3, column = 2, sticky=W)
+        tk.Label(self, text = "Setting").grid(row = 2, column = 2, sticky=tk.W)
+        tk.OptionMenu(self, self.setting, "Fantasy", "Sci-Fi", "IRL", 
+                   command = lambda x: self.updateMenus()).grid(row = 3, column = 2, sticky=tk.W)
 
-        Label(master, text = "Class").grid(row = 5, column = 2, sticky=W)
+        tk.Label(self, text = "Class").grid(row = 5, column = 2, sticky=tk.W)
         self.getClasses(self.setting.get())
 
-        Label(master, text = "Race").grid(row = 7, column = 2, sticky = W)
+        tk.Label(self, text = "Race").grid(row = 7, column = 2, sticky = tk.W)
         self.getRaces(self.setting.get())
 
-        Button(master, text="Submit", command = lambda: self.submit()).grid(row = 9, column = 1, sticky = W)
+        tk.Button(self, text="Submit", command = lambda: self.submit()).grid(row = 9, column = 1, sticky = tk.W)
 
     def updateMenus(self):
          self.getClasses(self.setting.get()) 
@@ -63,13 +63,13 @@ class App:
         classes = json.load(open("classes.json"))
         classList = classes[setting]
         self.classChoice.set(classList[0])
-        self.classMenu = OptionMenu(self.master, self.classChoice, *classList).grid(row = 6, column = 2, sticky = W)
+        self.classMenu = tk.OptionMenu(self, self.classChoice, *classList).grid(row = 6, column = 2, sticky = tk.W)
 
     def getRaces(self, setting):
         races = json.load(open("races.json"))
         raceList = races[setting]
         self.race.set(raceList[0])
-        self.raceMenu = OptionMenu(self.master, self.race, *raceList).grid(row = 8, column = 2, sticky = W)
+        self.raceMenu = tk.OptionMenu(self, self.race, *raceList).grid(row = 8, column = 2, sticky = tk.W)
 
     def submit(self):
         statChoices = []
@@ -83,20 +83,14 @@ class App:
 
         character = charGen.Character(self.system.get(), self.setting.get(),  self.classChoice.get(), self.race.get(), statChoices)
 
-        pop = PopUp(Tk(), character.stringCharacter)
+        strChar = character.stringCharacter()
+        self.PopUp(strChar)
 
-class PopUp:
-    def __init__(self, master, charString):
-        frame = Frame(master)
-        frame.grid()
+    def PopUp(self, stringCharacter):
+        t = tk.Toplevel(self)
+        tk.Label(t, text=stringCharacter).grid(row = 0, column = 0, sticky = tk.W)
+        tk.Button(t, text = "Close").grid(row = 1, column = 0, sticky = tk.W)
 
-        Text(master, charString).grid(row = 0, column = 0, sticky=W)
-        Button(master, "Close").grid(row = 1, column = 0, sticky=W)
-
-
-
-root = Tk()
-
-app = App(root)
-
-root.mainloop()
+app = App()
+app.master.title('Character')
+app.mainloop()
