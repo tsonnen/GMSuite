f75e5e84e9d566a407f4d4003f3f5336ca9f5b15
tsonnen <tsonnen67@gmail.com>
2016-06-21 20:09:58 -0700

    Add backend support for Ship Generator

--- ShipCreator/ui.py
+++ ShipCreator/ui.py
@@ -7,7 +7,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 import json
-#import shipCreator
+import shipCreator
 
 class shipCreatorUI:
     def __init__(self):
@@ -18,9 +18,12 @@ class shipCreatorUI:
 
         self.typeChoice = builder.get_object("typeChoice")
         self.sizeChoice = builder.get_object("sizeChoice")
+        self.submitButton = builder.get_object("submit")
 
         self.getTypes()
         self.getSizes()
+
+        self.submitButton.connect("clicked", self.submit_OnClick)
 
         window.show_all()
 
@@ -32,10 +35,17 @@ class shipCreatorUI:
         for i in types:
             self.typeChoice.append_text(i)
 
+        self.typeChoice.set_active(0)
+
     def getSizes (self):
         sizes = open("sizes.txt").read().splitlines()
 
         for i in sizes:
             self.sizeChoice.append_text(i)
+
+        self.sizeChoice.set_active(0)
+
+    def submit_OnClick(self, button):
+        ship = shipCreator.Ship(self.typeChoice.get_active_text(), self.sizeChoice.get_active_text())
 
 app = shipCreatorUI()
