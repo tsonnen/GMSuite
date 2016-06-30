db74a16db7586314e6abe6f96f3a766f1c0dab86
tsonnen <tsonnen67@gmail.com>
2016-06-29 21:10:59 -0700

    Add more options for ship generator

--- ShipCreator/shipCreator.py
+++ ShipCreator/shipCreator.py
@@ -1,19 +1,23 @@
-''' Tim Sonnen 6/21/2016
+''' Tim Sonnen 6/28/2016
     v.01
     Starship Generator for a tabletop RPG
 
     Creates a starship with the input of Type and size
 '''
 
-import time, sys, random, string, json
+import time
+import sys
+import random
+import string
+import json
 
 class Ship:
     def __init__(self, shipType):
         self.shipType = shipType
-        self.shipSize = ""
-        self.shipConfig = ""
 
         self.getShipInfo()
+        self.getArmor()
+        self.getSpecial()
 
     def getShipInfo(self):
         infoFile = open("types.json")
@@ -22,3 +26,23 @@ class Ship:
         self.shipSize = infoList[self.shipType]['size']
         self.shipClass = infoList[self.shipType]['class']
 
+    def getArmor(self):
+        armorFile = open("armor.json")
+        armorList = json.load(armorFile)
+        armorType = random.choice(armorList['armors'])
+
+        self.shipArmor = [armorType['typeName'], armorType['protection']]
+
+        armorFile.close()
+
+    def getSpecial(self):
+        self.shipSpecial = []
+
+        specialFile = open("special.txt")
+        specialList = specialFile.read().splitlines()
+
+        for line in specialList:
+            if random.randint(1, 100) > 50:
+                self.shipSpecial.append(line)
+
+        print(self.shipSpecial)
