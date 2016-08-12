020c9370a7949c1698d5efad6c975ba517017220
tsonnen <tsonnen67@gmail.com>
2016-08-11 19:24:59 -0700

    Improve the markov algorithm (know start and end of the names)

--- markov.py
+++ markov.py
@@ -1,7 +1,7 @@
-''' 7/13/2106
+'''  8/11/2016
 
     Generates a name based on a list of strings
-    using a MArkov chain algorithm
+    using a Markov chain algorithm
 '''
 
 import random
@@ -14,6 +14,7 @@ class markovname:
         self.order = order
 
         for i in self.nameList:
+            i = "\x02{0}\x03".format(i)
             self.load(i.lower())
 
     def load(self, s):
@@ -22,16 +23,29 @@ class markovname:
                 self.table[s[i:i + self.order]]
             except KeyError:
                 self.table[s[i:i + self.order]] = []
+
             self.table[s[i:i + self.order]] += s[i + self.order]
 
     def generate(self, start = None, max_length = 20):
         if start == None:
-            s = random.choice(list(self.table.keys()))
+            s = "s"
+            while(s[0] != "\x02"):
+                s = random.choice(list(self.table.keys()))
         else:
             s = start
         try:
+            nextVal = "nextVal"
             while len(s) < max_length:
-                s += random.choice(self.table[s[-self.order:]])
+                nextVal = random.choice(self.table[s[-self.order:]])
+                print(self.table[s[-self.order:]])
+                if(nextVal != "\x03"):
+                    s += nextVal
+                else:
+                    break
+
+                print(s)
+
+            s = s.replace("\x02", "")
         except KeyError:
             pass
         return s.capitalize()
